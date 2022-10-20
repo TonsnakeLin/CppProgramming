c6faa06bab29aa3796f31f89619359120435aac3
TonsnakeLin <lpbgytong@163.com>
2022-10-20 21:33:18 +0800

    add make file
    Signed-off-by: TonsnakeLin <lpbgytong@163.com>

---
+++ Makefile
@@ -0,0 +1,4 @@
+CXX=g++
+
+all: main.cc
+	g++  main.cc -o main -g -std=c++17
--- main.cc
+++ main.cc
@@ -1,3 +1,9 @@
+#include <stdio.h>
+#include <iostream>
+using std::cout;
+using std::endl;
+
 int main(int argc, char **argv) {
-	printf("hello word");
+	printf("hello world\n");
+	cout<<"hello world again"<<endl;
 }
