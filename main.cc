27b7cdbff82bcda04ee56c6a58cb6e00a6f5a60c
TonsnakeLin <lpbgytong@163.com>
2023-03-07 17:01:40 +0800

    test wakeup
    Signed-off-by: TonsnakeLin <lpbgytong@163.com>

--- main.cc
+++ main.cc
@@ -5,77 +5,17 @@
 #include <vector>
 #include <string>
 #include <algorithm>
+#include <thread>
+#include <chrono>
 
 using std::cout;
 using std::endl;
 using std::vector;
 using std::string;
 
-extern void* process_msg(void*);
-extern void enqueue_msg(void);
-extern vector<long> g_time_queue;
-
+extern void test_wakeup();
 
 int main(int argc, char **argv) {
-	pthread_t tidp;
-
-	if (pthread_create(&tidp, NULL, process_msg, NULL) == -1) {
-		printf("create error!\n");
-		return 1;
-	}
-
-	sleep(1);
-	for (int i=0; i < 100; i++) {
-		enqueue_msg();
-		sleep(1);
-	}
-
-	if (pthread_join(tidp, NULL)) {
-		printf("thread is not exit...\n");
-	}
-
-	double sum = 0;
-	int count = g_time_queue.size();
-	vector<int> wake_up_time;
-	for(int i = 0; i < g_time_queue.size(); i+=2) {
-		long t1 = g_time_queue[i];
-		long t2 = g_time_queue[i+1];
-		long delta = 0;
-	    if (t2 > t1) {
-			delta = t2 - t1;			
-	    } else {
-			delta = t1 - t2;	    	
-	    }
-		wake_up_time.push_back(delta);
-		sum += delta;
-	}
-	std::sort(wake_up_time.begin(), wake_up_time.end());
-	printf("avg: %f \n", 2*sum/count);
-
-	// string long_wake_up_string;
-	int over_40 =0, over_50 = 0, over_60 = 0;
-	int total = wake_up_time.size();
-	int p80 = int(total * 0.8);
-	int p95 = int(total * 0.95);
-	for (int i=0; i < wake_up_time.size(); i++) {
-		/*
-		if (i > 0) {
-			long_wake_up_string.append(", ");
-		}
-		
-		long_wake_up_string.append(std::to_string(wake_up_time[i]));
-		*/
-
-		if (wake_up_time[i] > 40 && wake_up_time[i] <= 50) {
-			over_40++;
-		} else if (wake_up_time[i] > 50 && wake_up_time[i] <= 60) {
-			over_50++;
-		} else if (wake_up_time[i] > 60) {
-			over_60++;
-		}
-	}
-	printf("40~50us count: %d, 50~60us count: %d, over 60us: %d \n", over_40, over_50, over_60);
-	printf("P80: %dus, P95: %dus, max: %dus\n", wake_up_time[p80], wake_up_time[p95], wake_up_time[total - 1]);
-
+	test_wakeup();
 	return 0;
 }
--- thread/thread.cc
+++ thread/thread.cc
@@ -4,7 +4,17 @@
 #include <sys/time.h>
 #include <unistd.h>
 #include <vector>
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <thread>
+#include <chrono>
+
+using std::cout;
+using std::endl;
 using std::vector;
+using std::string;
 
 bool workq;
 vector<long> g_time_queue;
@@ -29,13 +39,11 @@ void* process_msg(void*)
 	long t2 = 0;	
     for (int i=0; i < 100; i++) {
         pthread_mutex_lock(&qlock);
-        workq = true;
-	
-        pthread_cond_wait(&qready, &qlock);
-	
-		printf("process_msg timestamp: %ld \n", t2=get_current_time2());
+        workq = true;	
+        pthread_cond_wait(&qready, &qlock);	
+		t2=get_current_time2();
+		// printf("process_msg timestamp: %ld \n", t2);
 		g_time_queue.push_back(t2);
-
         pthread_mutex_unlock(&qlock);
     }
 }
@@ -50,7 +58,62 @@ void enqueue_msg(void)
 	t1 = get_current_time2();	
 	g_time_queue.push_back(t1);	
 	pthread_mutex_unlock(&qlock);
-	printf("enqueue_msg timestamp: %ld \n", t1);
+	// printf("enqueue_msg timestamp: %ld \n", t1);
+}
+
+int test_unit() {
+	pthread_t tidp;
+
+	if (pthread_create(&tidp, NULL, process_msg, NULL) == -1) {
+		printf("create error!\n");
+		return 1;
+	}
+
+	sleep(1);
+	for (int i=0; i < 100; i++) {
+		enqueue_msg();
+		// sleep(1);
+		std::this_thread::sleep_for(std::chrono::milliseconds(5));
+	}
+
+	if (pthread_join(tidp, NULL)) {
+		printf("thread does not exist ...\n");
+	}
+
+	double sum = 0;
+	int count = g_time_queue.size();
+	vector<int> wake_up_time;
+	for(int i = 0; i < g_time_queue.size(); i+=2) {
+		long t1 = g_time_queue[i];
+		long t2 = g_time_queue[i+1];
+		long delta = 0;
+	    if (t2 > t1) {
+			delta = t2 - t1;			
+	    } else {
+			delta = t1 - t2;	    	
+	    }
+		wake_up_time.push_back(delta);
+		sum += delta;
+	}
+	// printf("count:%d\n", count);
+	std::sort(wake_up_time.begin(), wake_up_time.end());
+	// printf("avg: %f \n", 2*sum/count);
+
+	// string long_wake_up_string;
+	int over_40 =0, over_50 = 0, over_60 = 0;
+	int total = wake_up_time.size();
+	int p50 = int(total * 0.5);
+	int p80 = int(total * 0.8);
+	int p99 = int(total * 0.99);
+
+	printf("avg: %f median: %dus, P99: %dus, max: %dus\n", 2*sum/count, wake_up_time[p50], wake_up_time[p99], wake_up_time[total - 1]);
+    return 0;
+}
+
+void test_wakeup() {
+	for (int i=0; i<10; i++) {
+		test_unit();
+	}
 }
 
 
