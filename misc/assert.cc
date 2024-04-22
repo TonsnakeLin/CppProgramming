70c8d179eec0417130e0402d1859bc0d86f2b046
TonsnakeLin <lpbgytong@163.com>
2024-04-22 03:20:30 -0400

    update
    Signed-off-by: TonsnakeLin <lpbgytong@163.com>

--- misc/assert.cc
+++ misc/assert.cc
@@ -4,8 +4,8 @@
 using std::cout;
 using std::endl;
 int test_assert_case1() {
-    // cout << "test_assert_case1\n";
-    assert(0);
+    cout << "test_assert_case1\n";
+    // assert(0);
     char *p = NULL;
     *p = 's';
     return 0;
